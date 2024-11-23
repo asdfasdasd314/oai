5118956239a7639ff7a45978fe06ffcd252077e9
James Hollingsworth <jameshollingsworth@Jamess-MacBook-Air.local>
2024-11-23 15:08:27 -0500

    Created a system to create valid recurrent times. Next feature will allow them to be added to a queue

--- gitSync.go
+++ gitSync.go
@@ -2,13 +2,84 @@ package main
 
 import (
 	"fmt"
-    "strings"
 	"strconv"
 	"time"
+	"strings"
+	"github.com/gen2brain/beeep"
 	"net/http"
 	"os/exec"
-	"github.com/gen2brain/beeep"
 )
+
+type CreateRecurrentTimeError int
+
+const (
+    InputFormat CreateRecurrentTimeError = iota // this starts at 0
+    ParseHours 
+    ParseMinutes
+    ParseSeconds
+    InvalidHours
+    InvalidMinutes
+    InvalidSeconds
+)
+
+func (e CreateRecurrentTimeError) Error() string {
+    switch e {
+        case InputFormat:
+            return "Invalid recurrent time format"
+        case ParseHours:
+            return "Could not parse hours string"
+        case ParseMinutes:
+            return "Could not parse minutes string"
+        case ParseSeconds:
+            return "Could not parse seconds string"
+        case InvalidHours:
+            return "Hours was not between 0 and 23"
+        case InvalidMinutes:
+            return "Minutes was not between 0 and 59"
+        case InvalidSeconds:
+            return "Seconds was not between 0 and 59"
+        default:
+            return "This error wasn't handled properly in CreateRecurrentTimeError"
+    }
+}
+
+type RecurrentTime struct {
+	Hours   int64
+	Minutes int64
+	Seconds int64
+}
+
+func NewRecurrentTime(timeAsStr string) (*RecurrentTime, error) {
+    splitTime := strings.Split(timeAsStr, ":")
+    if len(splitTime) != 3 {
+        return nil, InputFormat
+    }
+
+    hours, err := strconv.ParseInt(splitTime[0], 10, 0)
+    if err != nil {
+        return nil, ParseHours 
+    }
+    minutes, err := strconv.ParseInt(splitTime[1], 10, 0)
+    if err != nil {
+        return nil, ParseMinutes
+    }
+    seconds, err := strconv.ParseInt(splitTime[2], 10, 0)
+    if err != nil {
+        return nil, ParseSeconds 
+    }
+
+    if hours > 23 || hours < 0 {
+        return nil, InvalidHours 
+    }
+    if minutes > 59 || minutes < 0 {
+        return nil, InvalidMinutes
+    }
+    if seconds > 59 || seconds < 0 {
+        return nil, InvalidSeconds 
+    }
+
+    return &RecurrentTime{Hours: hours, Minutes: minutes, Seconds: seconds}, nil
+}
 
 func automaticGitSync(syncInterval time.Duration, checkTimeAccurateInterval time.Duration, retryGitSyncInterval time.Duration) {
 	// This one automatically syncs on the specified interval
@@ -41,40 +112,58 @@ func automaticGitSync(syncInterval time.Duration, checkTimeAccurateInterval time
 			nextSyncTime = time.Now().Add(syncInterval)
 		}
 	}()
+
 	// This syncs on user input
 	for {
 		var input string
 		fmt.Scanln(&input)
 
-        if strings.Contains(input, " ") { // In this case there are at least two words, so these ones involve the user trying to set something
-            // To do this switch well, we have to check the first word
-            words := strings.Split(input, " ")
-            firstWord := words[0]
-            switch firstWord {
-                case "set-sync-interval":
-                    // todo
-                    return 
-                case "set-recurrent-sync-time":
-                    // todo
-                    return
+		switch input {
+		// Immutable operatons //
+		case "sync":
+			fmt.Println("Syncing data...")
+			runGitSyncCommands(retryGitSyncInterval)
+			fmt.Println("Successfully synced data! | " + formatTime(time.Now()))
+		case "next-sync-time":
+			fmt.Println(formatTime(nextSyncTime))
+		case "time-until-sync":
+			fmt.Println(getTimeUntilSync(nextSyncTime))
+
+		// Mutable operations //
+		case "set-sync-interval":
+			// Read the hours, minutes, and seconds from the user
+			fmt.Print("Enter the number of hours, minutes and seconds (HH:MM:SS) in military time: ")
+			var recurringTimeString string
+			fmt.Scanln(&recurringTimeString)
+
+            var errorCreatingTime bool
+
+            recurrentTime, err := NewRecurrentTime(recurringTimeString)
+            if err != nil {
+                errorCreatingTime = true
             }
-        } else { // In this case there is only one word, and so the user is (usually) trying to get something
-            switch input {
-                case "sync":
-                    fmt.Println("Syncing data...")
-                    runGitSyncCommands(retryGitSyncInterval)
-                    fmt.Println("Successfully synced data! | " + formatTime(time.Now()))
-                case "next-sync-time":
-                    fmt.Println(formatTime(nextSyncTime))
-                case "time-until-sync":
-                    fmt.Println(getTimeUntilSync(nextSyncTime))
-                case "exit":
-                    // Wait to receive from the channel
-                    // This waits until there's something in the channel, which always before or after the automatic git sync commands are ran
-                    <-canExit
-                    return
+
+            for errorCreatingTime {
+                fmt.Println(err.Error())
+                fmt.Print("Enter the number of hours, minutes and seconds (HH:MM:SS) in military time: ")
+                fmt.Scanln(&recurringTimeString)
+
+                recurrentTime, err = NewRecurrentTime(recurringTimeString)
+                if err != nil {
+                    errorCreatingTime = true
+                }
             }
-        }
+
+            // Now we have a valid recurrent time object, we have to add to our binary tree now
+
+            panic("TODO")
+		// Exit //
+		case "exit":
+			// Wait to receive from the channel
+			// This waits until there's something in the channel, which always before or after the automatic git sync commands are ran
+			<-canExit
+			return
+		}
 	}
 }
 
@@ -112,6 +201,30 @@ func notifySuccess() {
 	err := beeep.Notify("Succesfully Synced with GitHub", "Success", "")
 	if err != nil {
 		panic(err)
+	}
+}
+
+// Takes the hours, minutes, and seconds of a military time that happens every day and maps it to the next closest occurence of that time
+// For example, if it were 6 PM on a certain day and 15 hours, 30 minutes, 30 seconds was given, then 3:30:30 PM on the next day would be returned
+func mapRecurrentTimeToTimestamp(hours int, minutes int, seconds int) int64 {
+	now := time.Now()
+	currHours := now.Hour()
+	currMinutes := now.Minute()
+	currSeconds := now.Second()
+
+	currTotalSeconds := currHours*60*60 + currMinutes*60 + currSeconds
+	recurrentTimeTotalSeconds := hours*60*60 + minutes*60 + seconds
+
+	currYear, currMonth, currDay := now.Date()
+	currLocation := now.Location()
+
+	recurrentTime := time.Date(currYear, currMonth, currDay, hours, minutes, seconds, 0, currLocation)
+	unixTime := recurrentTime.Unix()
+
+	if currTotalSeconds > recurrentTimeTotalSeconds { // Put the recurrent timestamp on the next day
+		return unixTime
+	} else { // Put the recurrent timestamp on the current day
+		return addDayToTime(unixTime)
 	}
 }
 
@@ -164,4 +277,9 @@ func getTimeUntilSync(syncTime time.Time) string {
 func isGithubAccessible() bool {
 	_, err := http.Get("https://www.github.com")
 	return err == nil
+}
+
+// Takes in the number of seconds since epoch and returns the corresponding time a day later
+func addDayToTime(unixTimestamp int64) int64 {
+	return unixTimestamp + 24*60*60
 }
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "time"
+	"time"
 )
 
 func main() {
-    sync := time.Duration(24 * time.Hour)
-    waitTime := time.Duration(5 * time.Second)
-    retryTime := time.Duration(15 * time.Minute)
-    automaticGitSync(sync, waitTime, retryTime)
+	sync := time.Duration(24 * time.Hour)
+	waitTime := time.Duration(5 * time.Second)
+	retryTime := time.Duration(15 * time.Minute)
+	automaticGitSync(sync, waitTime, retryTime)
 }
