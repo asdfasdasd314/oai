2c3d8e89be1d854412a8ff0af7464119203b5810
James Hollingsworth <jameshollingsworth@Jamess-MacBook-Air.local>
2024-12-02 09:50:58 -0500

    Actually I think I just fixed all the bugs...

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "time"
+	"fmt"
+	"os"
+	"time"
 )
 
 func main() {
-    // Then we have command line args passed into the program
-    inDebugMode := false
-    if len(os.Args) > 1 {
-        for _, arg := range os.Args {
-            if arg == "debug" {
-                fmt.Println("Running in debug mode")
-                inDebugMode = true
-            } 
-        }
-    }
-    retryInterval := time.Duration(2 * time.Minute)
-    verifyInterval := time.Duration(30 * time.Second)
-    RunApp(retryInterval, verifyInterval, inDebugMode)
+	// Then we have command line args passed into the program
+	inDebugMode := false
+	if len(os.Args) > 1 {
+		for _, arg := range os.Args {
+			if arg == "debug" {
+				fmt.Println("Running in debug mode")
+				inDebugMode = true
+			}
+		}
+	}
+	retryInterval := time.Duration(2 * time.Minute)
+	verifyInterval := time.Duration(1 * time.Second)
+	RunApp(retryInterval, verifyInterval, inDebugMode)
 }
-
